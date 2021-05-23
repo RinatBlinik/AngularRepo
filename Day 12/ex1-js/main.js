19d0ef40afdf622cbf47e6411008121a3692769a
RinatBlinik <82043211+RinatBlinik@users.noreply.github.com>
2021-05-23 15:48:10 +0300

    another try

---
+++ Day 12/ex1-js/main.js
@@ -0,0 +1,23 @@
+function Calculate(a,b)
+{
+    this.a=a;
+    this.b=b;
+}
+
+Calculate.prototype.add = function (){
+    return this.a + this.b;
+}
+Calculate.prototype.sub = function (){
+    return this.a - this.b;
+}
+Calculate.prototype.mult = function (){
+    return this.a * this.b;
+}
+Calculate.prototype.log = function (){
+    console.log('a + b = ' + this.add() + '   a - b = ' + this.sub()  + '   a * b = ' + this.mult() );
+}
+
+var c1= new Calculate(5,10);
+var c2= new Calculate(50,20);
+c1.log();
+c2.log();
